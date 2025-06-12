a8608f0f3b0a83592037d74515b4b60e59fdc460
Toni Förster <toni.foerster@icloud.com>
2025-06-12 16:29:41 +0200

    Fix: Prevent crash when canceling quit confirmation
    Resolves a crash when the user selects "No" in the "Quit to Library" modal.
    
    The crash was caused by the modal's callback attempting to modify a menu item that was already destroyed when the modal was presented.
    
    Signed-off-by: Toni Förster <toni.foerster@icloud.com>

--- src/game_scene.c
+++ src/game_scene.c
@@ -1451,7 +1451,6 @@ void PGB_LibraryConfirmModal(void *userdata, int option)
     }
     else
     {
-        playdate->system->setMenuItemValue(buttonMenuItem, 1);
         gameScene->button_hold_frames_remaining = 0;
         gameScene->button_hold_mode = 1;
         gameScene->audioLocked = false;
