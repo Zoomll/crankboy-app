ee4cbdd0ff23e6577e88697083457bfb1635d51f
Toni Förster <toni.foerster@icloud.com>
2025-06-15 22:06:56 +0200

    reordering
    Signed-off-by: Toni Förster <toni.foerster@icloud.com>

--- peanut_gb/peanut_gb.h
+++ peanut_gb/peanut_gb.h
@@ -5717,7 +5717,7 @@ uint_fast32_t gb_get_save_size(struct gb_s *gb)
         return 512;
 
     const uint_fast16_t ram_size_location = 0x0149;
-    const uint_fast32_t ram_sizes[] = {0x00, 0x800, 0x2000, 0x8000, 0x20000, 0x10000};
+    const uint_fast32_t ram_sizes[] = {0x00, 0x800, 0x2000, 0x8000, 0x10000, 0x20000};
     uint8_t ram_size = gb->gb_rom[ram_size_location];
     return ram_sizes[ram_size];
 }
